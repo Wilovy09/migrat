f155cde3950aa125cc12ab4b97abff48311a5963
Emiliano Maldonado Garza <srwilovy09@gmail.com>
2025-05-27 14:58:31 -0600

    ✨Testing how to make a request

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,38 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows" )]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+mod utils;
+use utils::request::make_request;
 
 use std::error::Error;
-
+use slint::Weak;
 slint::include_modules!();
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
+    let ui_handle: Weak<AppWindow> = ui.as_weak();
 
-    ui.on_request_decrease_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter()-1);
-        }
-    });
-
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter()+1);
-        }
-    });
+    {
+        let ui_weak = ui.as_weak();
+        ui.on_fetch_data(move || {
+            let ui = ui_weak.upgrade().expect("UI no longer exists");
+            let url = ui.get_endpoint().to_string();
+            let ui_handle = ui_handle.clone();
+            tokio::spawn(async move {
+                let response = make_request(url).await;
+                let text = match response {
+                    Ok(s) => slint::SharedString::from(s),
+                    Err(e) => slint::SharedString::from(format!("Error: {}", e)),
+                };
+                if let Err(e) = ui_handle.upgrade_in_event_loop(move |ui| {
+                    ui.set_response_text(text);
+                }) {
+                    eprintln!("Failed to upgrade UI in event loop: {:?}", e);
+                }
+            });
+        });
+    }
 
     ui.run()?;
-
     Ok(())
 }
